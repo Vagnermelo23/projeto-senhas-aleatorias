91a3ccc012c99984480cf71427913e8771dd1159
Vagner Ferreira de Melo <165937363+Vagnermelo23@users.noreply.github.com>
2024-04-07 19:55:10 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -1,18 +1,15 @@
 import random
 
-# Define os conjuntos de caracteres
 letras = 'qwertyuioplkjhgfdsamnbvcxz'
 numeros = '123456789'
 caracteres_especiais = '!@#$%¨&*'
 
-# Combina todos os conjuntos de caracteres
+
 todos_os_caracteres = letras + numeros + caracteres_especiais
 
-# Solicita o tamanho da senha ao usuário
 tamanho_da_senha = int(input("Digite o tamanho desejado para a senha: "))
 
-# Gera a senha aleatória
-senha = ''.join(random.choice(todos_os_caracteres) for _ in range(tamanho_da_senha))
+senha = ''.join(random.choice(todos_os_caracteres) for i in range(tamanho_da_senha))
 
-# Exibe a senha gerada
+
 print(f"Senha gerada: {senha}")
